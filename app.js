b09767f3acaca4882a89a4e4a36f502c0fd42f9e
COR Students <corstudents@Matts-MBP.localdomain>
2018-11-29 13:36:45 -0500

    express changes

--- app.js
+++ app.js
@@ -9,13 +9,21 @@ const app = express();
 //You set the port as well
 const port = 3000
 
+//setting pug as the default javascript viewing engine
+app.set('view engine', 'pug');
+
 //Here you set the app var to get data from the server when the instance of the app is ran.
 //The instance here is going to the page.
 app.get('/', (req, res) => {
 
+    //HTML rendoring into pug
+    res.render("index.pug")
+
     // The res parameter is then told to send back a response based on a good instance
-    res.sendFile(path.join(__dirname, '/views/index.html'));
+    // res.sendFile(path.join(__dirname, '/views/index.html'));
     app.use(express.static(path.join(__dirname, '/public')));
+    
+    // app.use(express.static(path.join(__dirname, '/pictures')))
 });
 
 //This is an example to visit another page (the about page)
@@ -26,7 +34,7 @@ app.get('/about', (req, res) => {
 });
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/contact.html'));
-    app.use(express.static(path.join(__dirname,'public')))
+   
 
 });
 //Setting the app var to listen on the port for an instance and if it happens run the function.
