b6d5047cd91e469ff3fa74d0dc790adaddb45bc4
Malachi Mccormick <malachimccormick@Malachis-MacBook-Pro.local>
2018-11-28 22:15:32 -0500

    setup express

---
+++ app.js
@@ -0,0 +1,33 @@
+//express can hide all important data from local clients to the server
+
+//first you set the var express to the express library
+const express = require('express');
+const path = require('path');
+// Here you set each instance of the app to express
+const app = express();
+
+//You set the port as well
+const port = 3000
+
+//Here you set the app var to get data from the server when the instance of the app is ran.
+//The instance here is going to the page.
+app.get('/', (req, res) => {
+
+    // The res parameter is then told to send back a response based on a good instance
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+    app.use(express.static(path.join(__dirname, '/public')));
+});
+
+//This is an example to visit another page (the about page)
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/about.html'));
+    app.use(express.static(path.join(__dirname,'/public')));
+
+});
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/contact.html'));
+    app.use(express.static(path.join(__dirname,'public')))
+
+});
+//Setting the app var to listen on the port for an instance and if it happens run the function.
+app.listen(port);
